b273c49c60a5cc252c3c5d316f4650070cfae8c6
Carl Duncan <cdsoftwaresja@gmail.com>
2021-04-20 13:17:07 -0500

    Changes to the statistics page

--- Client/wear/lib/pages/statistics.dart
+++ Client/wear/lib/pages/statistics.dart
@@ -17,68 +17,78 @@ class _StatisticsPageState extends State<StatisticsPage> {
       ),
       body: CustomScrollView(
         slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate.fixed([
-              Text(
-                "Latest updates for Jamaica",
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          height: 150,
-                          color: HexColor("#FFD058"),
+          SliverPadding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate.fixed([
+                Text(
+                  "Latest updates for Jamaica",
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            height: 150,
+                            color: HexColor("#FFD058"),
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          height: 150,
-                          color: HexColor("#FF5858"),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            height: 150,
+                            color: HexColor("#FF5858"),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            height: 150,
+                            color: HexColor("#58FFA5"),
+                          ),
                         ),
                       ),
                     ),
-                  )
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          height: 150,
-                          color: HexColor("#58FFA5"),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          child: Container(
+                            height: 150,
+                            color: HexColor("#B658FF"),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        child: Container(
-                          height: 150,
-                          color: HexColor("#B658FF"),
-                        ),
-                      ),
-                    ),
-                  )
-                ],
-              )
-            ]),
+                    )
+                  ],
+                ),
+                Text(
+                  "Daily confirmed cases",
+                ),
+                Container(
+                  height: 200,
+                  color: Colors.grey,
+                )
+              ]),
+            ),
           )
         ],
       ),
