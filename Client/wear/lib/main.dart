26c43c7d3ebd87de509577c1931befde2de164d4
Carl Duncan <cdsoftwaresja@gmail.com>
2021-05-12 15:47:02 -0500

    Minor Code Change to structure

--- Client/wear/lib/main.dart
+++ Client/wear/lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wear/pages/menu.dart';
-import 'package:wear/utils/colors.dart';
-import 'package:wear/utils/transition.dart';
-import 'package:wear/widget/row.dart';
+import 'package:wear/pages/home.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,106 +14,6 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String barcode = "";
-  Future scanCode() async {
-    try {
-      String barcode = await BarcodeScanner.scan();
-      setState(() => this.barcode = barcode);
-    } catch (e) {
-      if (e.code == BarcodeScanner.CameraAccessDenied) {
-        setState(() {
-          this.barcode = 'Camera permission was denied!';
-        });
-      } else {
-        setState(() => this.barcode = 'Unknown error: $e');
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CupertinoNavigationBar(
-        leading: Material(
-          child: IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              Navigator.push(context, createRoute(MenuPage()));
-            },
-          ),
-        ),
-        middle: Text('Dashboard'),
-        border: null,
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverList(
-            delegate: SliverChildListDelegate.fixed([
-              recentlyVisited(false),
-            ]),
-          ),
-          SliverPadding(
-            padding: const EdgeInsets.only(left: 20, right: 20),
-            sliver: SliverList(
-              delegate: SliverChildBuilderDelegate((context, index) {
-                return LocationRow(
-                  description1: "Description 1",
-                  description2: "Description 2",
-                  name: "Name of Location",
-                  count: 5,
-                  onTap: () {},
-                );
-              }, childCount: 5),
-            ),
-          )
-        ],
-      ),
-      // floatingActionButton: FloatingActionButton.extended(
-      //   onPressed: scanCode,
-      //   label: Text("Scan"),
-      //   icon: Icon(Icons.camera_alt),
-      // ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
-  }
-
-  recentlyVisited(bool completed) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ClipRRect(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        child: Container(
-          height: 50,
-          color: AppColors.error,
-          child: Center(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.place,
-                color: Colors.white,
-              ),
-              Text(
-                "Recently Visited",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                    fontSize: 14),
-              ),
-            ],
-          )),
-        ),
-      ),
     );
   }
 }
